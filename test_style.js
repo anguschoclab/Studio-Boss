e049fc2851d5f6fbfdef7ae1098d01027a1a46d4
anguschoclab <59230472+anguschoclab@users.noreply.github.com>
2026-04-03 11:16:16 +0200

    delete

--- test_style.js
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-
-const element = React.createElement('style', null, `</style><script>alert(1)</script>`);
-console.log(ReactDOMServer.renderToString(element));
