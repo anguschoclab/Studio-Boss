cce67952379e46db960f2dde817ce844cf1936f1
anguschoclab <59230472+anguschoclab@users.noreply.github.com>
2026-04-12 05:11:08 +0200

    Create index.ts

---
+++ src/components/layout/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Layout Components
+ * 
+ * Provides structural and layout components for consistent page organization
+ */
+
+export { Stack, VerticalStack, HorizontalStack, Space, Divider, Center } from './Stack';
+export { Section, SectionGroup, SubSection } from './Section';
+export { PageTransition, StaggerContainer, StaggerItem, AnimatedNumber } from './PageTransition';
+export { QuickActionsDock } from './QuickActionsDock';
+export { StudioSidebar } from './StudioSidebar';
+export { TopBar } from './TopBar';
+export { NewsTicker } from './NewsTicker';
